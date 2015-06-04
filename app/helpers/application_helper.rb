0e519c4f71c1bdad3f4a56047262c9f6decc8a21
dpalay <dpalay@rapodcast.net>
2015-06-04 00:21:33 -0500

    Fixing Git

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+def add_link provider, str, id
+  link_to_if(current_user.identities.find_by(provider: provider).nil?, "Add #{str}",
+             "/auth/#{provider}", id: id) do
+    "Already a #{str} Profile" end
+end
 end
